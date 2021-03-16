cc91394fef9ec2564f1593f7333e880fa287c804
Phil Bailey <philbailey@msn.com>
2021-03-16 12:03:10 -0400

    add step 2

--- step2.js
+++ step2.js
@@ -1,8 +1,17 @@
 "use strict";
-const axios = require(axios);
+const axios = require("axios");
 const fs = require("fs");
-let path = process.argv[2];
-cat(path);
+if (process.argv[2].includes("http")) {
+    let url = process.argv[2];
+    webCat(url);
+} else if (process.argv[2].includes(".")) {
+    let path = process.argv[2];
+    cat(path);
+} else {
+    console.log("Invalid Input");
+    process.exit(1);
+}
+
 // #######################################################
 function cat(path) {
     fs.readFile(path, "utf8", function (err, data) {
@@ -16,12 +25,12 @@ function cat(path) {
 }
 // #######################################################
 function webCat(url) {
-    axios.get(url).then(function (result) {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        } else {
-            console.log(data);
-        }
-    });
+    axios
+        .get(url)
+        .then(function (result) {
+            console.log(result.data);
+        })
+        .catch(function (e) {
+            console.log(e);
+        });
 }
